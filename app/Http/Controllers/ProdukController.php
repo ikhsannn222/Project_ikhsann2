fcbf115d48880330562b00522f5e77f578792169
ikhsannn222 <161899009+ikhsannn222@users.noreply.github.com>
2024-05-25 02:27:13 -0400

    Add files via upload

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Merk;
 use App\Models\Produk;
 use Illuminate\Http\Request;
 use PDF;
@@ -10,7 +11,7 @@ use Storage;
 class ProdukController extends Controller
 {
 
-          public function viewPDF()
+    public function exportPdf()
     {
         $produk = Produk::latest()->get();
 
@@ -24,125 +25,84 @@ class ProdukController extends Controller
             ->setPaper('a4', 'portrait');
         return $pdf->stream();
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        $produk = Produk::latest()->paginate(5);
-        return view ('produk.index', compact('produk'));
+        $produk = Produk::latest()->get();
+        return view('produk.index', compact('produk'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        return view('produk.create');
+        $merk = Merk::all();
+        return view('produk.create', compact('merk'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        // validate fromm
+        //validate form
         $this->validate($request, [
-            'nama'=> 'required|min:5',
-            'harga'=> 'required',
-            'image'=> 'required|image|mimes:jpeg,jpg,png|max:2048',
-            'deskripsi'=> 'required|min:10',
+            'nama' => 'required|min:5',
+            'harga' => 'required',
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'deskripsi' => 'required|min:10',
         ]);
 
         $produk = new Produk();
         $produk->nama = $request->nama;
         $produk->harga = $request->harga;
         $produk->deskripsi = $request->deskripsi;
+        $produk->id_merk = $request->id_merk;
         // upload image
         $image = $request->file('image');
         $image->storeAs('public/produks', $image->hashName());
         $produk->image = $image->hashName();
         $produk->save();
         return redirect()->route('produk.index');
-        }
+    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $produk = Produk::findOrFail($id);
-    return view('produk.show', compact('produk'));
-
+        return view('produk.show', compact('produk'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
+        $merk = Merk::all();
         $produk = Produk::findOrFail($id);
-        return view('produk.edit', compact('produk'));
+        return view('produk.edit', compact('produk', 'merk'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        $this->validate($request,[
-            'nama'=> 'required|min:5',
-            'harga'=> 'required|min:5',
-            'deskripsi'=> 'required|min:10',
+        $this->validate($request, [
+            'nama' => 'required|min:5',
+            'harga' => 'required',
+            'deskripsi' => 'required|min:10',
         ]);
 
         $produk = Produk::findOrFail($id);
         $produk->nama = $request->nama;
         $produk->harga = $request->harga;
         $produk->deskripsi = $request->deskripsi;
+        $produk->id_merk = $request->id_merk;
         // upload image
         if ($request->hasFile('image')) {
-        $image = $request->file('image');
-        $image->storeAs('public/produks', $image->hashName());
-        //delete old image
-        Storage::delete('public/produks/' . $produk->image);
-
-        $produk->image = $image->hashName();
+            $image = $request->file('image');
+            $image->storeAs('public/produks', $image->hashName());
+            Storage::delete('public/produks/' . $produk->image);
+            $produk->image = $image->hashName();
         }
-        
+
         $produk->save();
         return redirect()->route('produk.index');
 
-
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        $produk = Produk::FindOrFail($id);
-        Storage::delete('public/produks/'.$produk->image);
+        $produk = Produk::findOrFail($id);
+        Storage::delete('public/produks/' . $produk->image);
         $produk->delete();
         return redirect()->route('produk.index');
 
